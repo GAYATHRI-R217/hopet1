9324cab4eaa0a3d91cd38ea53146be38975ddbb1
GAYATHRI-R217 <gayathriramesh217@gmail.com>
2026-04-02 10:39:32 +0530

    day11

--- day11/exception.java
+++ day11/exception.java
@@ -15,8 +15,28 @@ package day11;
     // ->unchecked exception(runtime exception)-> null pointer exception, array index out of bound exception, arithmetic exception, number format exception
 //Program or environament around causing error
 //developers must handle this exception
+//try->Execute the risky part of the code
+//Catch->work when risky part of code fails
+//Finally->Always execute at last
+//throw->used to manually creating 
+//throws->used to declare the exception that a method can throw
 
-
-public class excepsion {
-    
+public class exception {
+    public static void main(String[] args) {
+        try{
+        /*int a=10;
+        int b=0;
+        int c=a/b;*/
+        String s=null;
+        System.out.println(s.length());
+    }catch (ArithmeticException e){
+        System.out.println("Cannot divide by zero.");
+    }
+        catch(NullPointerException e){
+            System.out.println("Null pointer exception occurred.");
+        }
+        finally{
+            System.out.println("This block will always execute.");
+        }
+    }
 }
