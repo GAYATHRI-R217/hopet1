4e377296b5c5a1a20e5350808b880539dc1983b6
GAYATHRI-R217 <gayathriramesh217@gmail.com>
2026-04-18 08:30:25 +0530

    day20

--- day20/Threads.java
+++ day20/Threads.java
@@ -1,5 +1,40 @@
 package day20;
-
+//Thread
+//Smallest unit of process that performs a specific task
+//Thread is a lightweight process
+//Thread is a part of process
+//Thread ->extends,thread class->run()
+// -> implements,runnable interface->run()
+//Thread class and Runnable interface are used to create a thread\
+//run() method is used to define the task that the thread will perform
+//start() method is used to start the thread
+/*class SampleThread extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Thread is running");
+    }
+}
 public class Threads {
-    
+    public static void main(String[] args) {
+        SampleThread t1 = new SampleThread();
+        t1.start();
+    }
+}
+    */
+public class Threads {
+     public static void main(String[] args) {
+        
+        task1();
+        task2();
+    }
+    static void task1(){
+        for(int i=0;i<5;i++){
+            System.out.println("From task1:"+i);
+        }
+    }
+    static void task2(){
+        for(int i=0;i<5;i++){
+            System.out.println("From task2:"+i);
+        }
+    }
 }
---
+++ day20/thread_example.java
@@ -0,0 +1,30 @@
+package day20;
+class SampleThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread running using Thread class");
+    }
+}
+
+class SampleThread2 implements Runnable {
+    @Override
+    public void run() { // Running state
+        try {
+            System.out.println("Thread running using Runnable");
+            Thread.sleep(4000); // Waiting State
+            System.out.println("After 4 seconds");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+public class thread_example {
+    public static void main(String[] args) {
+        SampleThread t = new SampleThread(); // New state
+        t.start(); // Runnable state
+        Thread t2 = new Thread(new SampleThread2());
+        t2.start();
+        // Terminated state
+    }
+}
